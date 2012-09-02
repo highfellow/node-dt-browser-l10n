4f3d17182c7a130bfd6cede398bad38b4ffa8614
Andy Baxter <highfellow@gmail.com>
2012-09-02 18:20:47 +0100

    more name changes

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-coffee -c src/dt-dom-l10n.coffee
-mv src/dt-dom-l10n.js .
+coffee -c src/dt-browser-l10n.coffee
+mv src/dt-browser-l10n.js .
 node_modules/.bin/browserify example/example.coffee -o example/example.js
